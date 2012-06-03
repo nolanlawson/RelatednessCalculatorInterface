672f6607bdd22bd07b5bc492c25fe419181988dc
Nolan Lawson <nolan.lawson@gmail.com>
2012-06-03 18:28:54 +0200

    initial commit

---
+++ grails-app/controllers/com/nolanlawson/relatedness/ui/CalculatorController.groovy
@@ -0,0 +1,12 @@
+package com.nolanlawson.relatedness.ui
+
+class CalculatorController {
+
+    def index() { 
+	
+	}
+	
+	def calculate() { 
+		
+	}
+}
---
+++ test/unit/com/nolanlawson/relatedness/ui/CalculatorControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.nolanlawson.relatedness.ui
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(CalculatorController)
+class CalculatorControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
