b1b0474759d84730075bd5070c1f31b83dfee9fe
Nolan Lawson <nolan.lawson@gmail.com>
2012-06-07 21:15:55 +0200

    tweak logging

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -88,7 +88,8 @@ log4j = {
 			'org.hibernate',
 			'net.sf.ehcache.hibernate'
 	warn   'org.mortbay.log'
-	info 'com.nolanlawson'
+	info 'com.nolanlawson',
+		'grails.app'
 
 	root {
 		info 'infoLog','stdout'
